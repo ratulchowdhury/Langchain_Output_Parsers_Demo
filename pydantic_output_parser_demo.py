197d649532067cdff7e45ab44c52ca0d6e08ebed
ratulchowdhury <ratulchowdhuryofficial@gmail.com>
2025-09-27 22:38:26 +0530

    Adding pydantic output parsers

--- pydantic_output_parser_demo.py
+++ pydantic_output_parser_demo.py
@@ -1,6 +1,7 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
-from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv  
 import os 
 load_dotenv()
@@ -16,12 +17,13 @@ llm = HuggingFaceEndpoint(
 
 llm = ChatHuggingFace(llm=llm, temperature = 0.5)
 
-schema = [
-    ResponseSchema(name="gameplay", description="a brief summary of the topic"),
-    ResponseSchema(name="advantages", description="the advantages of the topic, with examples of where it has been executed impeccably."),
-    ResponseSchema(name="tactical_flaws", description="the flaws of the topic, with examples of where the system has collapsed.")
-]
-parser = StructuredOutputParser.from_response_schemas(schema)
+class outputSchema(BaseModel):
+    gameplay :str = Field(description="a brief summary of the topic")
+    advantages :str = Field(description="the advantages of the topic, with examples of where it has been executed impeccably.")
+    tactical_flaws :str = Field(description="the flaws of the topic, with examples of where the system has collapsed.")
+    overall_rating :int = Field(gt = 0, lt = 10, description="Overall rating of the topic on a scale of 1 to 10")
+    
+parser = PydanticOutputParser(pydantic_object=outputSchema)
 template = PromptTemplate(template="""Consider yourself a football pundit. Now your task is to generate a summary, advantages, flaws of the following {topic}, \n {format_instructions}""",
                            input_variables = ["topic"],
                            partial_variables ={"format_instructions":parser.get_format_instructions()})
@@ -31,6 +33,7 @@ topic_name  = "catenaccio style of defense"
 result = chain.invoke({"topic":topic_name})
 print("Type of the output :", type(result))
 print("Output :", result)
-print(f"Gameplay of the {topic_name}:",result["gameplay"])
-print(f"Advantages of the {topic_name}:",result["advantages"])
-print(f"Flaws of the {topic_name}:",result["tactical_flaws"])
+print(f"Gameplay of the {topic_name}:",result.gameplay)
+print(f"Advantages of the {topic_name}:",result.advantages)
+print(f"Flaws of the {topic_name}:",result.tactical_flaws)
+print(f"Overall rating of the {topic_name}:",result.overall_rating)
